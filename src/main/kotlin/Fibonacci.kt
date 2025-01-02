6f7cd02da783549716ef858f2f0c9ba49d9348e4
andrew childre <andrewchildre@gmail.com>
2025-01-02 16:45:45 -0700

    update notes 1.2.25

--- src/main/kotlin/Fibonacci.kt
+++ src/main/kotlin/Fibonacci.kt
@@ -66,3 +66,49 @@ fun fibonacci3 (limit: Int, action: (Int) -> Unit) {
         current = prev + prevprev
     }
 }
+
+// Loop explanation
+//start with the values at the top
+//        temp = 1
+//        prevprev = 0
+//        prev = 0
+//        add prev and prevprev which is one
+//
+//        then the next loop starts
+//        temp = 1 which is the current number
+//        prevprev = 0
+//        prev = 1
+//        add prev + prevprev = 1
+//
+//        next loop is
+//        temp = 1 is the current
+//        prevprev = 1 is the previous prev
+//        prev = 1 is the temp
+//        add prev + prevprev = 2
+//
+//        next loop is
+//
+//        temp = 2 which is the current
+//        prevprev = 1 which is from the previous loop
+//        prev = 2 is the temp
+//        add prev + prevprev is 3
+//
+//        next loop is
+//
+//        temp = 3 add from the prevoius loop
+//        prevprev = 2 prev from the previous loop
+//        prev = 3 is the temp
+//        add prev + prevprev = 5
+//
+//        next loop is
+//
+//        temp = 5 which is the add from above (end of the list)
+//        prevprev = 3 is the prev from the prevoius loop (the one next to it)
+//        prev = 5 is the temp
+//        add prev + prevprev = 8
+//
+//        I have temp which is the value from the previous loop its the last entry in the list of numbers
+//        I have prevprev which is the prev from the prevoius loop
+//        I have prev which is now the temp
+//        then I add prev and prevprev
+
